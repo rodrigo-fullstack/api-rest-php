2ef87136d41f7fd848e5d790e151f541c056b8c1
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-18 01:18:57 -0300

    feat (req-res): Requisição e Resposta para o Core

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -4,5 +4,7 @@
 namespace App\Http;
 
 Class Request{
-    
+    public static function method(){
+        return $_SERVER['REQUEST_METHOD'];
+    }
 }
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -5,4 +5,12 @@ namespace App\Http;
 
 Class Response{
     
+    public static function json($data = [], $status = 200, $headers){
+        http_response_code($status);
+        
+        header('Content-type-application: application/json');
+
+        echo json_encode($data);
+    }
+
 }
