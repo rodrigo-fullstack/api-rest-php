0567ab899049469f25662715534b0018f647f752
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-18 14:38:53 -0300

    feat (routes): Adicionando novas rotas para a aplicação
    Foram adicionadas novas rotas para execução de operações CRUD (Create, Read, Update e Delete)

--- src/routes/main.php
+++ src/routes/main.php
@@ -6,5 +6,10 @@ use App\Http\Routes;
 // Padronização do Action é o nome da controladora separada por @ e o método da controladora
 Routes::get("/", "HomeController@index");
 Routes::get("/index", "HomeController@index");
+Routes::get('/users/login', "UserController@login");
+Routes::get('/users/fetch', "UserController@fetch");
+Routes::post("/users/save", "UserController@save");
+Routes::put('/users/update', "UserController@update");
+Routes::delete('/users/{id}/delete', "UserController@delete");
 
-// $routes = Routes::routes();
+// $routes = Routes::routes(');
