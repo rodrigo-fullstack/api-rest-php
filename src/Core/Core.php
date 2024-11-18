c40927e0dc59974c1d65e3f001c3b22d428498b0
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-18 00:44:00 -0300

    feat (core): Melhorando core para analisar url da api

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -6,6 +6,38 @@ namespace App\Core;
 Class Core{
     // URLs
     public static function dispatch(array $routes){
-        print$routes;
-    }
+        // dump($routes);
+
+        $url = "/";
+
+        $namespaceController = "App\\Controllers\\";
+
+        // Se houver url além de /, concatena na $url com essa nova url
+        isset($_GET['url']) && $url .= $_GET['url'];
+
+        foreach($routes as $route){
+            // RegExpression para determinar quando há id na url, convertendo para alfanumérico
+            // Está aceitando passar id pelo url
+            $pattern = '#^' . preg_replace('/{id}/', '([\w-]+)', $route['path']) . '$#';
+
+            if(preg_match($pattern, $url, $matches)){
+                // Impede de retornar o path
+                array_shift($matches);
+
+                //Separa o action yyyyController do método a partir do @
+                [$controller, $action] = explode('@', $route['action']);
+
+                // Coleta o nome completo do namespace da controller
+                $controller =  $namespaceController . $controller;
+                // instancia a controladora
+                $extendController = new $controller;
+
+                // Executa o método de acordo com o que foi passado em $action;
+                $extendController->$action();
+
+                // dump($matches);
+            }
+        }
+
+    }   
 }
--- src/routes/main.php
+++ src/routes/main.php
@@ -5,8 +5,6 @@ use App\Http\Routes;
 // Determinando rotas com os métodos estáticos
 // Padronização do Action é o nome da controladora separada por @ e o método da controladora
 Routes::get("/", "HomeController@index");
-Routes::post("/", "HomeController@index");
-Routes::put("/", "HomeController@index");
-Routes::delete("/", "HomeController@index");
+Routes::get("/index/{id}", "HomeController@index");
 
 // $routes = Routes::routes();
