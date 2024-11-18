8d9e1ff4ae9cd330feb8e7af2271e98632239279
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-18 03:15:38 -0300

    feat (routeNFound): Quando a rota não é encontrada

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -5,6 +5,7 @@ namespace App\Core;
 
 use App\Http\Request;
 use App\Http\Response;
+use App\Controllers;
 
 Class Core{
     // URLs
@@ -20,6 +21,8 @@ Class Core{
         // Se houver url além de /, concatena na $url com essa nova url
         isset($_GET['url']) && $url .= $_GET['url'];
 
+        $url !== "/" && $url = rtrim($url, '/');
+
         foreach($routes as $route){
             // RegExpression para determinar quando há id na url, convertendo para alfanumérico
             // Está aceitando passar id pelo url
@@ -28,8 +31,8 @@ Class Core{
             if(preg_match($pattern, $url, $matches)){
                 // Impede de retornar o path
                 array_shift($matches);
+                $routeFound = true;
 
-                
 
                 // Se o método for diferente da requisição ao servidor, gera um erro 405
                 // Corrigir dps erro do método POST passar na requisição
@@ -58,11 +61,19 @@ Class Core{
                 $extendController = new $controller;
 
                 // Executa o método de acordo com o que foi passado em $action;
-                $extendController->$action();
+                $extendController->$action(new Request, new Response, $matches);
 
                 // dump($matches);
             }
         }
 
+        if(!$routeFound){
+            $controller = $namespaceController . 'NotFoundController';
+            
+            $extendController = new $controller;
+
+            // Injeção de Dependência
+            $extendController->index(new Request, new Response);
+        }
     }   
 }
