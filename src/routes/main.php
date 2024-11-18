647326cf2a73e739be87bd6102bbf806cf42508c
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-18 03:17:23 -0300

    fix (routes): Consertando rota para o que era antes

--- src/routes/main.php
+++ src/routes/main.php
@@ -5,6 +5,6 @@ use App\Http\Routes;
 // Determinando rotas com os métodos estáticos
 // Padronização do Action é o nome da controladora separada por @ e o método da controladora
 Routes::get("/", "HomeController@index");
-Routes::get("/index/{id}", "HomeController@index");
+Routes::get("/index", "HomeController@index");
 
 // $routes = Routes::routes();
