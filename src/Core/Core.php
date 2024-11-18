225696135cd8b6f07247898397e634112d3f85a0
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-17 22:02:30 -0300

    first commit

---
+++ src/Core/Core.php
@@ -0,0 +1,8 @@
+<?php
+
+
+namespace App\Core;
+
+Class Core{
+    // URLs
+}
---
+++ src/Http/Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http;
+
+Class Routes{
+    public static $routes = [
+        
+    ];
+
+
+}
