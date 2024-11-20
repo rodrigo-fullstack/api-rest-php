4e8b87651a720fa1226099207915dcb595856912
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-20 09:07:09 -0300

    fix: Modificando forma de chamar o BD

--- src/Models/User.php
+++ src/Models/User.php
@@ -5,16 +5,23 @@ namespace App\Models;
 use App\Models\Database;
 
 class User{
-    public static function save(array $data){
-        // Com conexão estática e o pdo acessível internamente nos métodos
-        Database::query("
+    public static function save($data){
+        $bd = new Database();
+        
+        // Consulta de inserção...
+        $bd->query("
             INSERT INTO usuario(email, senha)
             VALUES(:email, :senha)
         ");
 
-        Database::bind(':email', $data['email']);
-        Database::bind(':senha', $data['senha']);
+        // Vincula os parâmetros determinados na inserção
+        $bd->bind(':email', $data['email']);
+        $bd->bind(':senha', $data['senha']);
 
-        Database::execute();
+        // Execução da consulta
+        $bd->execute();
+
+        // Retorna se houve última inserção de ID, caso contrário é um false
+        return $bd->lastInsertId();
     }
 }
