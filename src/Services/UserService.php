4b859773fe71a1b4918710a2e2d2130b9c633221
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-11-21 14:46:17 -0300

    feat (validate-errors): Função de Validar erros do PDO

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -17,9 +17,10 @@ class UserService{
             // Envia senha criptografada para o validator
             $fields = Validator::validate([
                 "email" => $data['email'] ?? '',
-                "senha" => password_hash($data['senha'], PASSWORD_BCRYPT) ?? ''
+                "senha" => $data['senha'] ?? ''
             ]);
 
+            $fields['senha'] = password_hash($fields['senha'], PASSWORD_BCRYPT);
             // Cadastro do dado, retorna se houve inserção ou não
             // Caso houve erro no pdo, gera erro no catch
             $user = User::save($fields);
@@ -32,24 +33,19 @@ class UserService{
         }
         catch(PDOException $e){
             echo "Passei no catch do PDO...";
-            $dsnError = "There was an error with your PDO...";
-            $sintaxError = "There was an error with your SQL Sintax... ";
-            $errorToJson = match($e->getCode()){
-                2002 => ["error" => "$dsnError It can be your dsn host or db port."],
-                1049 => ["error" => "$dsnError It can be your dbname."],
-                "42S02" => ["error" => "$sintaxError It can be your table name."],
-                "42000" => ["error" => "$sintaxError It can be your sql statement."],
-                "42S22" => ["error" => "$sintaxError It can be your table attributes that aren't correct"],
-                "HY093" => ["error" => "$sintaxError It can be a problem with the param's binding"],
-                "23000" => ["error" => "Problem with the sent value, maybe it was null or repeat of an unique key"],
-                $e->getCode() => ["error" => "Code error: {$e->getCode()}" ]
-
-            };
-            return $errorToJson;
+            
+            return Validator::validatePDO($e->getCode());
         }
         catch(Exception $e){
             echo "Passei no catch da Exception...";
-            return $e->getMessage();
+            // Corrigido código 201 para quando não recebe senha ou email válido
+            return ["error" => $e->getMessage()];
         }
     }
+
+    public static function update(){
+
+    }
+
+    
 }
--- src/Utils/Validator.php
+++ src/Utils/Validator.php
@@ -17,4 +17,23 @@ class Validator{
 
         return $fields;
     }
+
+    public static function validatePDO($codeNum){
+        $dsnError = "There was an error with your PDO...";
+        $sintaxError = "There was an error with your SQL Sintax... ";
+        $msgError = match($codeNum){
+            2002 => ["error" => "$dsnError It can be your dsn host or db port."],
+            1049 => ["error" => "$dsnError It can be your dbname."],
+            "42S02" => ["error" => "$sintaxError It can be your table name."],
+            "42000" => ["error" => "$sintaxError It can be your sql statement."],
+            "42S22" => ["error" => "$sintaxError It can be your table attributes that aren't correct."],
+            "HY093" => ["error" => "$sintaxError It can be a problem with the param's binding."],
+            "23000" => ["error" => "Problem with the sent value, maybe it was null or repeat of an unique key."],
+            0 => ["error" => "$dsnError Check if your dbms is correct."],
+            $codeNum => ["error" => "Code error: {$codeNum}."]
+
+        };
+
+        return $msgError;
+    }
 }
